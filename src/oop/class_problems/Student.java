b2e7d66020e8eb78323809a8fb03de926210ab5c
kullkarnitanuj <designommetallics@gmail.com>
2026-08-28 11:51:08 +0530

    Added Q5 Student

---
+++ src/oop/class_problems/Student.java
@@ -0,0 +1,29 @@
+package oop.class_problems;
+
+public class Student {
+
+    String name;
+    int attendance;
+
+    static String collegeName = "SRM Institute of Science and Technology";
+    static int studentCount = 0;
+
+    public Student(String name, int attendance) {
+        this.name = name;
+        this.attendance = attendance;
+        studentCount++;
+    }
+
+    static void printCollegeInfo() {
+        System.out.println("College: " + collegeName);
+        System.out.println("Students created: " + studentCount);
+    }
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student("Tanuj", 90);
+        Student s2 = new Student("Argha", 85);
+
+        Student.printCollegeInfo();
+    }
+}
